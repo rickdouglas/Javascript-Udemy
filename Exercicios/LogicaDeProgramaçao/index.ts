edc00522b842848d3ff86732445c97c3ce5eaaea
rickdouglas <rickmiranda23@gmail.com>
2022-07-24 18:25:12 -0300

    usando operador ternario

--- Exercicios/LogicaDeProgramaçao/index.ts
+++ Exercicios/LogicaDeProgramaçao/index.ts
@@ -1,15 +1,10 @@
 // Funçao que retorna o maior numero entre 2 numeros
 
 function retornaMaior (x, y) {
-    if (x > y){
-        return console.log(`O maior numero é ${x}`)
-    }else {
-        return console.log(`O maior numero é ${y}`)
-    }
+    x > y ? console.log(`O numero maior é ${x}`) : console.log(`O numero maior é ${y}`);
 }
 
 let numero1 = 150;
 let numero2 = 20
 
-const resultado  = retornaMaior(numero1, numero2);
-console.log(resultado);
+console.log(retornaMaior(numero1, numero2));
