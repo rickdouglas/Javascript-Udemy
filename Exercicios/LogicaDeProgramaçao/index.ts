dd40e9a72e13e5149a508665233076e09e1b5c31
rickdouglas <rickmiranda23@gmail.com>
2022-07-24 18:29:59 -0300

    Otimizando codigo usando arrow function

--- Exercicios/LogicaDeProgramaçao/index.ts
+++ Exercicios/LogicaDeProgramaçao/index.ts
@@ -1,10 +1,4 @@
 // Funçao que retorna o maior numero entre 2 numeros
 
-function retornaMaior (x, y) {
-    return x > y ? console.log(`O numero maior é ${x}`) : console.log(`O numero maior é ${y}`);
-}
-
-let numero1 = 150;
-let numero2 = 20
-
-console.log(retornaMaior(numero1, numero2));
+const maxNumber = (x, y) => x > y ? console.log(`O numero maior é ${x}`) : console.log(`O numero maior é ${y}`);
+console.log(maxNumber(130, 250));
