2169a5a8bf2ce23519b465ead6eaa888e6c91bac
rickdouglas <rickmiranda23@gmail.com>
2022-07-24 18:38:49 -0300

    Treinando Logica

--- Exercicios/LogicaDeProgramaçao/index.ts
+++ Exercicios/LogicaDeProgramaçao/index.ts
@@ -1,4 +1,8 @@
 // Funçao que retorna o maior numero entre 2 numeros
+// Funcçao que retorna se uma imagem esta no modo paisagem
 
 const maxNumber = (x, y) => x > y ? console.log(`O numero maior é ${x}`) : console.log(`O numero maior é ${y}`);
 console.log(maxNumber(130, 250));
+
+const ePaisagem = (width, height) => width > height ? console.log(`A imagem está no modo paisagem`) : console.log(`A imagem está no modo retrato`);
+console.log(ePaisagem(1920, 1080))
