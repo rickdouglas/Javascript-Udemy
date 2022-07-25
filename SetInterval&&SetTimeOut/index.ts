aba9bb869d54cafdd33fe8b273eb115227ff609c
rickdouglas <rickmiranda23@gmail.com>
2022-07-24 22:09:17 -0300

    SetTimeOut e SetInterval

---
+++ SetInterval&&SetTimeOut/index.ts
@@ -0,0 +1,8 @@
+function mostraHora(){
+    let data = new Date()
+    return data.toLocaleTimeString('pt-BR');
+}
+
+const timer = setInterval(function(){console.log(mostraHora())}, 1000);
+
+setTimeout(function(){clearInterval(timer)}, 5000)
