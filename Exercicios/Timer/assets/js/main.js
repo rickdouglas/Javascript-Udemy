be2079de9ebaa0911b5539f7bfe19b57742fb3c5
rickdouglas <rickmiranda23@gmail.com>
2022-08-14 17:14:41 -0300

    ajustes

--- Exercicios/Timer/assets/js/main.js
+++ Exercicios/Timer/assets/js/main.js
@@ -1,36 +1,42 @@
-function createTimer(seconds){
-  const data =  new Date(seconds * 1000);
-  return data.toLocaleTimeString('pt-BR', {
-    hour12:false,
-    timeZone: 'UTC'
-  })
+function TimeCounter () {
+  
+  function createTimer(seconds){
+    const data =  new Date(seconds * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+      hour12:false,
+      timeZone: 'UTC'
+    })
+  }
+  
+  const timeCounter = document.querySelector('.timer')
+  let seconds = 0;
+  let timer;
+  
+  function startTimer(){
+    timer = setInterval(function(){
+      seconds++;
+      timeCounter.innerHTML = createTimer(seconds);
+    }, 1000)
+  }
+  
+  document.addEventListener('click' , (event) => {
+    const element = event.target;
+    if (element.classList.contains('start')){
+      timeCounter.classList.remove('paused')
+      clearInterval(timer);
+      startTimer();
+    }
+    else if (element.classList.contains('pause')){
+      timeCounter.classList.add('paused')
+      clearInterval(timer);
+    }
+    else if (element.classList.contains('restart')){
+      timeCounter.classList.remove('paused')
+      clearInterval(timer);
+      timeCounter.innerHTML = '00:00:00';
+      seconds = 0;
+    }
+    else console.log('Invalid option')
+  })  
 }
-
-const timeCounter = document.querySelector('.timer')
-const start = document.querySelector('.start')
-const pause = document.querySelector('.pause')
-const restart = document.querySelector('.restart')
-let seconds = 0;
-let timer;
-
-function startTimer(){
-  timer = setInterval(function(){
-    seconds++;
-    timeCounter.innerHTML = createTimer(seconds);
-  }, 1000)
-}
-
-start.addEventListener('click', function(event){
-  timeCounter.classList.remove('paused')
-  clearInterval(timer);
-  startTimer();
-});
-pause.addEventListener('click', function(event){
-  timeCounter.classList.add('paused')
-  clearInterval(timer);
-})
-restart.addEventListener('click', function(event){
-  clearInterval(timer);
-  timeCounter.innerHTML = '00:00:00';
-  seconds = 0;
-})
+TimeCounter();
